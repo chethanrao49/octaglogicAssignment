155ca2c3c497a8a2be25c54f07fadf4d162cc9ac
chethanrao49 <41532730+chethanrao49@users.noreply.github.com>
2021-09-09 20:07:53 +0530

    Api to get all avaliable cars

--- src/app/controllers/carController.js
+++ src/app/controllers/carController.js
@@ -12,6 +12,20 @@ class CarController {
       res.status(500).send('Server Error');
     }
   }
+
+  async index2(req, res) {
+    try {
+      const cars = await Car.find({
+        avaliable: true
+      })
+      .lean()
+      res.status(201).send({ success: true, cars });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+
 }
 
 export default new CarController();
